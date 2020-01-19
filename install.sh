5a580d37d446e91efda8f55db2779a1781cf512e
Jeremy Meyer <jeremypatrickmeyer@gmail.com>
2020-01-18 22:22:08 -0800

    Update install and git alias

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 
 source ~/dotfiles/shell/.function
 
-# for file in .{bash_profile,gitconfig}; do
-#   [ -r "$HOME/dotfiles/$file" ] && rm $HOME/$file && ln -s $HOME/dotfiles/$file $HOME/$file
-# done
-# unset file
-
 for file in $HOME/dotfiles/home/.[!.]*; do
   if [ -r "$file" ] && ([ -f "$file" ] || [ -d "$file" ]) ; then
     mkdir -p ~/dotfiles/bak/home/
@@ -25,7 +20,6 @@ done
 unset file
 
 include ~/.bash_profile
-# include ~/.zshrc
 
-reset
+reload
 
